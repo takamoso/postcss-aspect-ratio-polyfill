51b217b1543e718503bf8b22a52739d66cb6ac7d
takamoso <htmlcssjslove@gmail.com>
2021-03-01 13:47:22 +0900

    Support PostCSS v8

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const postcss = require('postcss')
 const np = require('number-precision')
 
-module.exports = postcss.plugin('postcss-aspect-ratio-polyfill', options => css => {
-  css.walkDecls('aspect-ratio', decl => {
-    const rule = decl.parent
-    const selector = rule.selector
-    const beforeRule = postcss.rule({selector: `${selector}::before`, raws: {after: rule.raws.after, semicolon: rule.raws.semicolon}})
-    const afterRule = postcss.rule({selector: `${selector}::after`, raws: {before: rule.raws.after, after: rule.raws.after, semicolon: rule.raws.semicolon}})
-    const ratio = decl.value.replace(/['"]?((?:\d*\.?\d*)?)(?:\s*[\:\|\/]\s*)(\d*\.?\d*)['"]?/g, (match, width, height) => np.times(np.divide(height, width), 100) + '%')
+module.exports = () => {
+  return {
+    postcssPlugin: 'postcss-aspect-ratio-polyfill',
+    Declaration: {
+      'aspect-ratio': (decl, {Rule, Declaration}) => {
+        const rule = decl.parent
+        const selector = rule.selector
+        const beforeRule = new Rule({selector: `${selector}::before`, raws: {after: rule.raws.after, semicolon: rule.raws.semicolon}})
+        const afterRule = new Rule({selector: `${selector}::after`, raws: {before: rule.raws.after, after: rule.raws.after, semicolon: rule.raws.semicolon}})
+        const ratio = decl.value.replace(/['"]?((?:\d*\.?\d*)?)(?:\s*[\:\|\/]\s*)(\d*\.?\d*)['"]?/g, (match, width, height) => np.times(np.divide(height, width), 100) + '%')
+
+        beforeRule.append([
+          new Declaration({prop: 'float', value: 'left', raws: decl.raws}),
+          new Declaration({prop: 'padding-top', value: ratio}),
+          new Declaration({prop: 'content', value: "''"}),
+        ])
+        afterRule.append([
+          new Declaration({prop: 'display', value: 'block'}),
+          new Declaration({prop: 'content', value: "''"}),
+          new Declaration({prop: 'clear', value: 'both'}),
+        ])
     
-    beforeRule.append([
-      postcss.decl({prop: 'float', value: 'left', raws: decl.raws}),
-      postcss.decl({prop: 'padding-top', value: ratio}),
-      postcss.decl({prop: 'content', value: "''"}),
-    ])
-    afterRule.append([
-      postcss.decl({prop: 'display', value: 'block'}),
-      postcss.decl({prop: 'content', value: "''"}),
-      postcss.decl({prop: 'clear', value: 'both'}),
-    ])
+        rule.after(afterRule)
+        rule.after(beforeRule)
+    
+        rule.nodes.length === 1 ? rule.remove() : decl.remove()
+      },
+    },
+  }
+}
 
-    rule.after(afterRule)
-    rule.after(beforeRule)
-
-    rule.nodes.length === 1 ? rule.remove() : decl.remove()
-  })
-})
+module.exports.postcss = true

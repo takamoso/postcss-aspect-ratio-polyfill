1e8c29da184dff38fdc0a76ffb5017cbc02d49fe
Dylan Awalt-Conley <web@dylan.ac>
2022-04-22 01:05:02 -0400

    removed before and after raws to improve spacing

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ module.exports = () => {
         const rule = decl.parent
         const selector = rule.selector
         const supportsRule = new AtRule({name: 'supports', params: 'not (aspect-ratio: 1 / 1)'})
-        const beforeRule = new Rule({selector: `${selector}::before`, raws: {after: rule.raws.after, semicolon: rule.raws.semicolon}})
-        const afterRule = new Rule({selector: `${selector}::after`, raws: {before: rule.raws.after, after: rule.raws.after, semicolon: rule.raws.semicolon}})
+        const beforeRule = new Rule({selector: `${selector}::before`, raws: {semicolon: rule.raws.semicolon}})
+        const afterRule = new Rule({selector: `${selector}::after`, raws: {semicolon: rule.raws.semicolon}})
         const ratio = decl.value.replace(/['"]?((?:\d*\.?\d*)?)(?:\s*[\:\|\/]\s*)(\d*\.?\d*)['"]?/g, (match, width, height) => np.times(np.divide(height, width), 100) + '%')
 
         beforeRule.append([
-          new Declaration({prop: 'float', value: 'left', raws: decl.raws}),
+          new Declaration({prop: 'float', value: 'left'}),
           new Declaration({prop: 'padding-top', value: ratio}),
           new Declaration({prop: 'content', value: "''"}),
         ])
